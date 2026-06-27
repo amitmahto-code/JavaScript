9b1b7b61e6a18e8d5dc9c87f0ebfc7d84f4166c8
Mahto Amit <mahtoamit6678@gmail.com>
2026-06-27 21:15:29 +0530

    mini project card

---
+++ Addtional exercies/events and event handling/card/card.js
@@ -0,0 +1,45 @@
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let main = document.querySelector("#main");
+form.addEventListener("submit", function(detls){
+    detls.preventDefault();
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let profile =document.createElement("div");
+    profile.classList.add("profile");
+    
+    let img = document.createElement("img");
+    img.classList.add("img");
+    img.setAttribute("src",inputs[0].value);
+
+
+    let h3 = document.createElement("h3");
+    // h3.classList.add("card h3");
+    h3.textContent = inputs[1].value;
+    
+    let h5 = document.createElement("h5");
+    // h5.classList.add("card h5");
+    h5.textContent = inputs[2].value;
+    
+    let p = document.createElement("p");
+    // p.classList.add("card p");
+    p.textContent = inputs[3].value;
+
+    profile.appendChild(img);
+    card.appendChild(profile);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(p);
+
+    
+    main.appendChild(card);
+    
+    inputs.forEach(function(inp){
+        if (inp.type !== "submit"){
+            inp.value = "";
+        }
+    });
+
+});
